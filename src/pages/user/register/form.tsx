dac7dd20fc3a9420b62cc946838d28bf7bb4e763
liang xiaobeibei <290352095@qq.com>
2019-01-20 11:46:36 +0800

    ts类型修正

--- src/pages/user/register/form.tsx
+++ src/pages/user/register/form.tsx
@@ -1,53 +1,67 @@
-import React from 'react'
-import {Input, InputAdornment, IconButton, Button, FormControl, Select, MenuItem, InputLabel} from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
-import {Visibility, VisibilityOff} from '@material-ui/icons'
+import React from 'react';
+import {
+  Input,
+  InputAdornment,
+  IconButton,
+  Button,
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+  StyleRulesCallback,
+} from '@material-ui/core';
+import { StyleRules, withStyles } from '@material-ui/core/styles';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 
 export interface Props {
-
+  classes: { [key: string]: string };
 }
 
 interface State {
   showPassword: boolean;
   showEnterPassword: boolean;
-  params: object;
+  params: {
+    [key: string]: any;
+  };
   sendCode: boolean;
+  [key: string]: any;
 }
 
 const StyledSelect = withStyles({
   icon: {
-    display: 'none'
-  }
+    display: 'none',
+  },
 })(Select);
 
 const styles = theme => ({
   form: {
-    padding: '0px 50px 50px 50px'
+    padding: '0px 50px 50px 50px',
   },
   formControl: {
     minWidth: 120,
     width: '100%',
   },
   formSelectControl: {
-    width: 'calc(50% - 6px)'
+    width: 'calc(50% - 6px)',
   },
   formLeftSelectControl: {
-    marginRight: 6
+    marginRight: 6,
   },
   formRightSelectControl: {
-    marginLeft: 6
+    marginLeft: 6,
   },
   input: {
     height: 60,
     marginTop: '0px !important',
     icon: {
-      fontSize: 40
-    }
+      fontSize: 40,
+    },
   },
   button: {
     minWidth: 115,
-    textAlign: 'right',
-    color: '#FF40AEA8'
+    TextAlign: 'right',
+    // textAlign: 'right',
+    color: '#FF40AEA8',
   },
   submitButton: {
     marginTop: 50,
@@ -55,52 +69,52 @@ const styles = theme => ({
     color: '#FFFFFF',
     width: '100%',
     backgroundColor: '#40AEA8',
-    fontSize: 20
-  }
-})
+    fontSize: 20,
+  },
+});
 class RegisterForm extends React.Component<Props, State> {
-
   constructor(props: Props) {
-    super(props)
+    super(props);
     this.state = {
       showPassword: false,
       showEnterPassword: false,
       sendCode: false,
       params: {
         exp: '',
-        role: ''
-      }
-    }
+        role: '',
+      },
+    };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.params)
-  }
+    console.log(this.state.params);
+  };
 
   sendCode = () => {
     this.setState({
-      sendCode: true
-    })
-  }
+      sendCode: true,
+    });
+  };
 
   handleClickShowPassword(name: string) {
+    // eslint:disable //
     this.setState({
-      [name]: !this.state[name]
-    })
+      [name]: !this.state[name],
+    });
   }
 
-  inputChange = (e) => {
-    const {name, value} = e.target
+  inputChange = e => {
+    const { name, value } = e.target;
     this.setState({
-      params: Object.assign({}, this.state.params, {[name]: value})
-    })
-  }
+      params: Object.assign({}, this.state.params, { [name]: value }),
+    });
+  };
 
   render() {
     const { classes } = this.props;
-    const { showPassword, showEnterPassword, sendCode, params } = this.state
-    const { role, exp } = params
+    const { showPassword, showEnterPassword, sendCode, params } = this.state;
+    const { role, exp } = params;
     return (
       <div className="register">
         <form onSubmit={this.handleSubmit} className={classes.form}>
@@ -119,10 +133,9 @@ class RegisterForm extends React.Component<Props, State> {
               placeholder="输入6位验证码"
               onChange={this.inputChange}
               endAdornment={
-                <Button
-                  onClick={this.sendCode}
-                  className={classes.button}
-                >{`${sendCode ? '重新': ''}获取验证码`}</Button>
+                <Button onClick={this.sendCode} className={classes.button}>{`${
+                  sendCode ? '重新' : ''
+                }获取验证码`}</Button>
               }
             />
           </FormControl>
@@ -135,9 +148,7 @@ class RegisterForm extends React.Component<Props, State> {
               onChange={this.inputChange}
               endAdornment={
                 <InputAdornment position="end">
-                  <IconButton
-                    onClick={this.handleClickShowPassword.bind(this, 'showPassword')}
-                  >
+                  <IconButton onClick={this.handleClickShowPassword.bind(this, 'showPassword')}>
                     {showPassword ? <Visibility /> : <VisibilityOff />}
                   </IconButton>
                 </InputAdornment>
@@ -171,7 +182,7 @@ class RegisterForm extends React.Component<Props, State> {
             />
           </FormControl>
           <FormControl className={`${classes.formSelectControl} ${classes.formLeftSelectControl}`}>
-            {role ? null: <InputLabel shrink={false}>选择角色</InputLabel>}
+            {role ? null : <InputLabel shrink={false}>选择角色</InputLabel>}
             <StyledSelect
               name={'role'}
               value={role}
@@ -186,8 +197,10 @@ class RegisterForm extends React.Component<Props, State> {
               <MenuItem value={50}>小程序</MenuItem>
             </StyledSelect>
           </FormControl>
-          <FormControl className={`${classes.formSelectControl} ${classes.formRighttSelectControl}`}>
-            {exp ? null: <InputLabel shrink={false}>工作经验</InputLabel>}
+          <FormControl
+            className={`${classes.formSelectControl} ${classes.formRighttSelectControl}`}
+          >
+            {exp ? null : <InputLabel shrink={false}>工作经验</InputLabel>}
             <StyledSelect
               name={'exp'}
               value={exp}
@@ -206,9 +219,13 @@ class RegisterForm extends React.Component<Props, State> {
           </Button>
         </form>
       </div>
-    )
+    );
   }
 }
 
+export default withStyles(styles)(RegisterForm);
 
-export default withStyles(styles)(RegisterForm)
+// tslint:disable //——忽略该行以下所有代码出现的错误提示
+// tslint:enable //——当前ts文件重新启用tslint
+// tslint:disable-line——忽略当前行代码出现的错误提示
+// tslint:disable-next-line——忽略下一行代码出现的错误提示
