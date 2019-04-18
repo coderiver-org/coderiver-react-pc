5c26aa18d10b916f53e77f559d60de5b5a615d29
Fed.Akax <fed.akax@gmail.com>
2019-04-18 21:47:15 +0800

    feat: update basic

--- src/component/common/Avatar/index.tsx
+++ src/component/common/Avatar/index.tsx
@@ -5,8 +5,14 @@ import { Avatar } from 'antd';
 export default class BasicHeader extends Component {
   render() {
     return (
-      <div>
+      <div className={styles['avatar-contanier']}>
         <Avatar />
+        <Avatar icon="user" />
+        <Avatar size="large">U</Avatar>
+        <Avatar>USER</Avatar>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+        <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+        <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
       </div>
     );
   }
--- src/component/common/Input/index.tsx
+++ src/component/common/Input/index.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+
 import styles from './index.less';
-import { Menu, Icon } from 'antd';
+import { Input } from 'antd';
 
 export default class BasicHeader extends Component {
   render() {
-    return <div>this is empty</div>;
+    return (
+      <div className={styles['input-container']}>
+        <Input />
+      </div>
+    );
   }
 }
