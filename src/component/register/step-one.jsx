ff03153389dbd120ae6b0e7369b7f07321c632f0
Fed.Akax <fed.akax@gmail.com>
2019-05-08 22:29:35 +0800

    feat: register info step one

--- src/component/register/step-one.jsx
+++ src/component/register/step-one.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './style.less';
 import { Form, Button, Row, Col } from 'antd';
-import { Input } from 'component/common';
-import InputCaptcha from './captcha-input';
+import { Input, Select } from 'component/common';
 const Item = Form.Item;
 
 @Form.create()
@@ -19,31 +18,19 @@ export default class Index extends React.Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
-
     const config = {
       rules: [{ required: true, message: '请输入!' }],
     };
 
     return (
       <Form layout="vertical" onSubmit={this.handleSubmit} className={styles['style-form']}>
-        <Item label="邮箱或手机">{getFieldDecorator('user', config)(<Input />)}</Item>
+        <Item label="选择角色">{getFieldDecorator('user', config)(<Select />)}</Item>
 
-        <Item label="用户名">{getFieldDecorator('user1', config)(<Input />)}</Item>
-
-        <Item label="密码">{getFieldDecorator('user2', config)(<Input type="password" />)}</Item>
-
-        <Item label="重复密码">
-          {getFieldDecorator('user222', config)(<Input type="password" />)}
-        </Item>
-
-        <Item label="输入验证码">{getFieldDecorator('user12', config)(<InputCaptcha />)}</Item>
+        <Item label="工作经验">{getFieldDecorator('user1', config)(<Select />)}</Item>
 
         <Row gutter={6} className={styles['btn-group']}>
           <Col span={8}>
-            <Button type="primary">注册</Button>
-          </Col>
-          <Col span={8}>
-            <Button>直接登录</Button>
+            <Button type="primary">继续</Button>
           </Col>
         </Row>
       </Form>
